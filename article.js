bf4922bbb27dbf7a7ff37ac6ee29a92106c9179c
makary-jamal <121382109+makary-jamal@users.noreply.github.com>
2024-06-17 22:00:02 +0300

    Add files via upload

---
+++ article.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    url: String,
+    photo_link: String,
+  },
+  {
+    timestamps: true,
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const Article = require("./article");
+
+const app = express();
+
+// Connect to MongoDB
+const uri =
+  "mongodb+srv://makary_gamal:HT9VwfAJE0JdkJev@cluster0.n13ua1m.mongodb.net/?retryWrites=true&w=majority&appName=test";
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// Create a new article
+app.post("/", async (req, res) => {
+  const article = new Article({
+    title: "Article 1",
+    description: "Description 1",
+    url: "https://www.example.com",
+    photo_link: "https://www.example.com/photo.jpg",
+  });
+
+  try {
+    const newArticle = await article.save();
+    res.status(201).json({
+      status: "success",
+      data: newArticle,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err,
+    });
+  }
+});
+
+// Get all articles
+app.get("/", async (req, res) => {
+  try {
+    const articles = await Article.find();
+    res.status(200).json({
+      status: "success",
+      length: articles.length,
+      data: articles,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "error",
+      message: err,
+    });
+  }
+});
+
+// Get an article by ID
+app.get("/:id", async (req, res) => {
+  try {
+    const article = await Article.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: article,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "error",
+      message: err,
+    });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`);
+});
